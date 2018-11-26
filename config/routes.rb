97cc19877f4d14947a8730f66a55ad858e649898
Jean Charles Fabre <jcharlesfabre@gmail.com>
2018-11-26 16:54:35 +0100

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,10 @@
 Rails.application.routes.draw do
-  get 'bookings/udpate'
-  get 'bookings/new'
-  get 'bookings/edit'
-  get 'bookings/index'
-  get 'bookings/show'
-  get 'bookings/destroy'
-  get 'bookings/create'
-  get 'games/show'
-  get 'games/edit'
-  get 'games/new'
-  get 'games/destroy'
-  get 'games/index'
-  get 'games/update'
+
   devise_for :users
-  get 'pages/home'
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :games do
+    resources :bookings, only: [:new,:create]
+  end
+  resources :bookings, only: [:destroy, :show, :update, :edit, :index]
+
 end
