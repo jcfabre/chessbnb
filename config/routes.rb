0e1f4e4885e547f76152fa8129cabb8a4d6ba411
Jean Charles Fabre <jcharlesfabre@gmail.com>
2018-11-26 16:36:47 +0100

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  get 'bookings/udpate'
+  get 'bookings/new'
+  get 'bookings/edit'
+  get 'bookings/index'
+  get 'bookings/show'
+  get 'bookings/destroy'
+  get 'bookings/create'
+  get 'games/show'
+  get 'games/edit'
+  get 'games/new'
+  get 'games/destroy'
+  get 'games/index'
+  get 'games/update'
   devise_for :users
   get 'pages/home'
   root to: 'pages#home'
