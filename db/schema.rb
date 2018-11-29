28ba120ccadbc2577ad3d4bc64c834d25bec7491
Jean Charles Fabre <jcharlesfabre@gmail.com>
2018-11-29 17:13:30 +0100

    added schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2018_11_29_132053) do
 
   # These are extensions that must be enabled in order to support this database
