bcb0182c4d681c984a6769813f336d18aed1add2
Jean Charles Fabre <jcharlesfabre@gmail.com>
2018-11-30 10:39:04 +0100

    added profile pics

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,61 +2,67 @@ puts "Destroying all records..."
 Booking.destroy_all
 Game.destroy_all
 User.destroy_all
-
+TimeControl.destroy_all
 
 require 'faker'
 
 addresses = ["11 rue Lamartine Paris", "146 avenue parmentier Paris", "3 rue de rivoli Paris", "Place d'armes Versailles", "61 Route des Tribunes 75016 Paris"]
+
+# Create time controls
 
 time_control1 = TimeControl.create(name: "Blitz", duration: 5)
 time_control2 = TimeControl.create(name: "Rapid", duration: 20)
 time_control3 = TimeControl.create(name: "Classic", duration: 100)
 array_time_control = [time_control1, time_control2, time_control3]
 
-user1 = User.new(email: "jcharlesfabre@gmail.com",
-                rating: 1700,
-                first_name: "jc",
-                last_name: "fabre",
-                remote_photo_url: "https://chessbase.in/images/20161003_pic_0953_jpg")
-user1.password = 'valid_password'
-user1.password_confirmation = 'valid_password'
-user1.save!
+# Create users
 
-user2 = User.new(email: "toto@gmail.com",
-  first_name: "toto",
-  last_name: "Alfred",
-  rating: 1550,
-  remote_photo_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Yu_Yangyi_2012.jpg/800px-Yu_Yangyi_2012.jpg"
-)
-user2.password = 'valid_password'
-user2.password_confirmation = 'valid_password'
-user2.save!
+user1 = User.create!(email: "paul@cheam.io",
+                rating: 2300,
+                first_name: "Paul",
+                last_name: "Cheam",
+                remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543570145/paul.png",
+                password: 'password',
+                password_confirmation: 'password'
+                )
 
+user_attributes = [
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569551/images_12.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569551/images_7.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569551/images_8.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569551/images_10.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569551/images_9.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569551/images_6.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569551/images_5.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569551/images_11.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569552/images_4.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569554/images_3.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569554/download_13.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569555/download_12.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569555/download_11.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569555/download_10.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569556/images_2.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569556/images_1.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569557/images.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569557/download_9.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569558/download_8.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569558/download_7.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569558/download_6.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569559/download_5.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569560/download_4.jpg"},
+  {remote_photo_url: "https://res.cloudinary.com/doojphhb7/image/upload/v1543569561/download_3.jpg"},
+]
 
-user3 = User.new(
-  email: "connor.bubbles@yahoo.com",
-  first_name: "connor",
-  last_name: "bubbles",
-  rating: 2300,
-  remote_photo_url:"https://cdn2.listsoplenty.com/listsoplenty-cdn/uploads/2011/06/kateryna_lahno.jpg"
-)
-user3.password = 'ikv.q7KT_hS)'
-user3.password_confirmation = 'ikv.q7KT_hS)'
-user3.save!
-
-10.times do
-  first_name = Faker::Superhero.prefix
-  last_name = Faker::Ancient.god
-
+user_attributes.each do |attribute|
   User.create!(
-    first_name: first_name,
-    last_name: last_name,
+    first_name: Faker::Internet.username(5..8),
+    last_name: Faker::Ancient.god,
     rating: rand(1001..2835),
-    remote_photo_url: "https://cdn.cnn.com/cnnnext/dam/assets/181128205455-magnus-carlsen-super-tease.jpg",
-    email: Faker::Internet.email("#{first_name} #{last_name}" , '.'),
-    password: 'valid_password'
+    remote_photo_url: attribute[:remote_photo_url],
+    email: Faker::Internet.email,
+    password: 'password',
+    password_confirmation: 'password'
   )
-
   2.times do
     game = Game.new(
       title: Faker::GreekPhilosophers.quote,
