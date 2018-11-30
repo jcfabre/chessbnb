f16490926ee7dd7fd9f9cb973b1a92e82bc11fc6
paul cheam <paulccheam@gmail.com>
2018-11-30 15:25:28 +0100

    typed banner

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,5 +2,7 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 import "../plugins/flatpickr";
+import { loadDynamicBannerText } from '../components/banner';
 
 initMapbox();
+loadDynamicBannerText();
