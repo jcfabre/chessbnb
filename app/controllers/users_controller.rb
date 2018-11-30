ae110be0e7b97ee2d4841b828cf4ca06a00bb680
Jean Charles Fabre <jcharlesfabre@gmail.com>
2018-11-30 15:18:28 +0100

    removed current user in profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :index]
 
   def show
-    @user = current_user
+    # @user = current_user
+
     authorize @user
   end
 
